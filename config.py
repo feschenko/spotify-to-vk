15990bc749bbac4483f81b013b9a668c2602aebd
MazZz3R <linus.gb.1996@gmail.com>
2020-08-22 17:22:30 +0300

    feat: add config file

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+
+class Config:
+    STANDART_STATUS = os.getenv("STANDART_STATUS") or "Your Status"
+    STATUS = os.getenv("STATUS") or "⏯ ʟɪsᴛᴇɴɪɴɢ ᴏɴ sᴘᴏᴛɪꜰʏ: {track} - {artist} - {album}"
+    VK_TOKEN = os.getenv("VK_TOKEN") or "Your Token"
+    CLIENT_ID = os.getenv("CLIENT_ID") or ""
+    CLIENT_SECRET = os.getenv("CLIENT_SECRET") or ""
+    REDIRECT_URI = os.getenv("REDIRECT_URI") or ""
+    USERNAME = os.getenv("USERNAME") or "Username"
+    SCOPE = os.getenv("SCOPE") or "user-read-playback-state user-library-read"
