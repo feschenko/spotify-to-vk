e32b15cd9a4ffa39ce806bc0b640f2e378e42352
MazZz3R <linus.gb.1996@gmail.com>
2020-08-22 17:23:12 +0300

    refact: edit url for vk token

--- first.py
+++ first.py
@@ -2,7 +2,7 @@ import webbrowser
 import time
 
 first_url = "https://developer.spotify.com/dashboard/"
-second_url = "https://vkhost.github.io"
+second_url = "https://oauth.vk.com/authorize?client_id=2685278&scope=1073737727&redirect_uri=https://oauth.vk.com/blank.html&display=page&response_type=token&revoke=1"
 third_url = 'https://www.youtube.com/watch?v=RuxZaDHeZpA'
 
 
@@ -10,7 +10,5 @@ def open():
     webbrowser.open(url=first_url)
     webbrowser.open(url=second_url)
     webbrowser.open(url=third_url)
-
-
 
 open()
