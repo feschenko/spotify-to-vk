589d056c6099f609aa849a8a5a7cb56ce5333777
Kirill Feschenko <swipduces@yandex.com>
2020-12-10 22:23:08 +0300

    Fixed a lot of bugs. Removed delay before a status update.

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 class Config:
     STANDARD_STATUS = "_"
-    STATUS = "{track} - {artist} - {album}"  # Template for status text.
+    STATUS = "{track} - {artist} - {album}"
     VK_TOKEN = ""  # Access token for VKontakte account.
     CLIENT_ID = ""  # Client ID for spotify application.
     CLIENT_SECRET = ""  # Client secret for spotify application.
-    REDIRECT_URI = ""  # Redirect uri for spotify application.
-    USERNAME = ""  # Your name in spotify..
+    REDIRECT_URI = "http://localhost:8888"  # Redirect uri for spotify application.
+    USERNAME = ""  # Your name in spotify.
     SCOPE = "user-read-playback-state user-library-read"  # Spotify scopes.
--- main.py
+++ main.py
@@ -1,52 +1,51 @@
-from spotipy.oauth2 import SpotifyOAuth
-from typing import List
-
+from spotipy.oauth2 import SpotifyOAuth as sOauth
+import spotipy as sp
 from config import Config
 from colorama import Fore
-import spotipy
-import typing
 import vk_api
 import time
 
+
+spotify = sp.Spotify(auth_manager=sOauth(scope=Config.SCOPE, client_id=Config.CLIENT_ID,
+                                         client_secret=Config.CLIENT_SECRET, redirect_uri=Config.REDIRECT_URI,
+                                         username=Config.USERNAME))
+
+
 vk = vk_api.VkApi(token=Config.VK_TOKEN).get_api()
-
-spotify = spotipy.Spotify(
-    auth_manager=SpotifyOAuth(
-        scope=Config.SCOPE,
-        client_id=Config.CLIENT_ID,
-        client_secret=Config.CLIENT_SECRET,
-        redirect_uri=Config.REDIRECT_URI,
-        username=Config.USERNAME,
-    )
-)
-
-current_playing = typing.List[typing.Union[str, str, str]]
+user = vk.account.getProfileInfo()
+current_playing = []
 
 
 def update_status_to_standard():
-    if vk.users.get(fields="status")[0]["status"] != Config.STANDARD_STATUS:
+    if vk.status.get(user_id=user["id"]) != Config.STANDARD_STATUS:
         vk.status.set(text=Config.STANDARD_STATUS)
-    print(Fore.RED + f"User status was changed to {Config.STANDARD_STATUS}")
+        print(Fore.GREEN + "Your status was changed to a standard because of error.")
 
 
-def update_status(_current_playing: typing.List[typing.Union[str, str, str]]
-                  ) -> typing.List[typing.Union[str, str, str]]:
-    current = spotify.current_user_playing_track()
-    track, album, artist = current["item"]["name"], \
-                           current["item"]["album"]["name"], \
-                           current["item"]["artists"][0]["name"]
-    if _current_playing != [track, album, artist]:
-        vk.status.set(text=Config.STATUS.format(track=track, album=album, artist=album))
-        print(Fore.GREEN + f"Now playing: * {track} * {album} * {artist}")
-    if _current_playing is None:
-        raise
-    return [track, album, artist]
+def update_status(last_playing: list) -> list:
+    current_playing = spotify.current_user_playing_track()
+
+    if not current_playing:
+        update_status_to_standard()
+        return []
+
+    current_playing = [current_playing["item"]["name"], current_playing["item"]["album"]["name"],
+                       current_playing["item"]["artists"][0]["name"]]
+    if current_playing != last_playing:
+        search_result = vk.audio.search(q=f'{current_playing[0]} {current_playing[2]}')
+        if search_result["count"] == 0:
+            vk.status.set(text=Config.STATUS.format(track=current_playing[0], album=current_playing[1],
+                                                    artist=current_playing[2]))
+        else:
+            vk.audio.setBroadcast(audio=f"{search_result['items'][0]['owner_id']}_{search_result['items'][0]['id']}")
+        print(Fore.GREEN + f"Now playing: * {current_playing[0]} * {current_playing[1]} * {current_playing[2]}")
+    return current_playing
 
 
 while True:
     try:
         current_playing = update_status(current_playing)
-        time.sleep(5)
+        time.sleep(0.5)
     except (KeyboardInterrupt, SystemExit, Exception):
         update_status_to_standard()
         raise
