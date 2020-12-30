0aa78fb64ac087a46edfa6fe0aa03190cc91d1fc
vvia <kupryianchykdanya@gmail.com>
2020-12-30 20:12:55 +0300

    remove standard_status, status template, username; add user-read-recently-played scope

--- config.py
+++ config.py
@@ -1,9 +1,6 @@
 class Config:
-    STANDARD_STATUS = "_"
-    STATUS = "{track} - {artist} - {album}"
     VK_TOKEN = ""  # Access token for VKontakte account.
     CLIENT_ID = ""  # Client ID for spotify application.
     CLIENT_SECRET = ""  # Client secret for spotify application.
     REDIRECT_URI = "http://localhost:8888"  # Redirect uri for spotify application.
-    USERNAME = ""  # Your name in spotify.
-    SCOPE = "user-read-playback-state user-library-read"  # Spotify scopes.
+    SCOPE = "user-read-playback-state user-read-recently-played"  # Spotify scopes.
