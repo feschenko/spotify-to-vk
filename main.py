d46c388795aeb55527bed93e0118a759b219563f
vvia <kupryianchykdanya@gmail.com>
2020-12-30 20:14:02 +0300

    add recently played track displayed in status; small refactoring

--- main.py
+++ main.py
@@ -1,59 +1,60 @@
 from spotipy.oauth2 import SpotifyOAuth as sOauth
 import spotipy as sp
+import vk_api
 from config import Config
 from colorama import Fore
-import vk_api
 import time
+import random
 
 
-spotify = sp.Spotify(auth_manager=sOauth(scope=Config.SCOPE, client_id=Config.CLIENT_ID,
-                                         client_secret=Config.CLIENT_SECRET, redirect_uri=Config.REDIRECT_URI,
-                                         username=Config.USERNAME))
-
-
+spotify = sp.Spotify(auth_manager=sOauth(scope=Config.SCOPE,
+                                         client_id=Config.CLIENT_ID,
+                                         client_secret=Config.CLIENT_SECRET,
+                                         redirect_uri=Config.REDIRECT_URI))
 vk = vk_api.VkApi(token=Config.VK_TOKEN).get_api()
-user = vk.account.getProfileInfo()
-current_playing = []
+status_data = []
 
 
-def update_status_to_standard():
-    if vk.status.get(user_id=user["id"])["text"] != Config.STANDARD_STATUS:
-        vk.status.set(text=Config.STANDARD_STATUS)
-        print(Fore.RED + "Your status was changed to a standard because of error.")
+def update_status(last_status_data: list) -> list:
 
-
-def update_status(last_playing: list) -> list:
     current_playing = spotify.current_user_playing_track()
+    emoji = ['☔', '⚡']
 
-    if current_playing is None:
-        update_status_to_standard()
-        return current_playing
+    if current_playing is None or current_playing["currently_playing_type"] == "ad" or\
+            current_playing["is_playing"] is False:
 
-    if current_playing["currently_playing_type"] != "ad":
-        if current_playing["is_playing"] is False:
-            update_status_to_standard()
-            return current_playing
-
-        current_playing = [current_playing["item"]["name"], current_playing["item"]["album"]["name"],
-                           current_playing["item"]["artists"][0]["name"]]
-
-        if current_playing != last_playing:
-            search_result = vk.audio.search(q=f"{current_playing[2]} {current_playing[0]}")
-            if search_result["count"] == 0:
-                vk.status.set(text=Config.STATUS.format(track=current_playing[0], album=current_playing[1],
-                                                        artist=current_playing[2]))
-            else:
-                vk.audio.setBroadcast(audio=f"{search_result['items'][0]['owner_id']}_{search_result['items'][0]['id']}")
-            print(Fore.GREEN + f"Now playing: * {current_playing[0]} * {current_playing[1]} * {current_playing[2]}")
+        recently_played = spotify.current_user_recently_played(limit=50)
+        rp_track = recently_played['items'][-1]
+        status_data = [rp_track['track']['name'],
+                       rp_track['track']['album']['name'],
+                       ', '.join([el['name'] for el in rp_track["track"]["artists"]])]
+        if status_data != last_status_data:
+            text = f'{random.choice(emoji)}Spotify recently: {status_data[0]} {random.choice(emoji)} {status_data[1]} ' \
+                   f'{random.choice(emoji)} {status_data[2]} {random.choice(emoji)}'
+            vk.status.set(text=text)
+            print(Fore.YELLOW + text)
     else:
-        update_status_to_standard()
-    return current_playing
+        status_data = [current_playing["item"]["name"],
+                       current_playing["item"]["album"]["name"],
+                       ', '.join(el['name'] for el in current_playing["item"]["artists"])]
 
+        if status_data != last_status_data:
+            search_result = vk.audio.search(q=f"{status_data[2]} {status_data[0]}")
+            if search_result["count"] == 0:
+                text = f'{random.choice(emoji)}Spotify playing: {status_data[0]} {random.choice(emoji)} {status_data[1]} ' \
+                       f'{random.choice(emoji)} {status_data[2]} {random.choice(emoji)}'
+                vk.status.set(text=text)
+                print(text)
+            else:
+                vk.audio.setBroadcast(
+                    audio=f"{search_result['items'][0]['owner_id']}_{search_result['items'][0]['id']}")
+            print(Fore.GREEN + f"Now playing: * {status_data[0]} * {status_data[1]} * {status_data[2]}")
+    return status_data
 
 
 while True:
     try:
-        current_playing = update_status(current_playing)
+        status_data = update_status(status_data)
+        time.sleep(1)
     except (KeyboardInterrupt, SystemExit):
-        update_status_to_standard()
         raise
