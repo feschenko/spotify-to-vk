992dabbb29e417da9ffaa7db9c8dffa015a0e7a4
Kirill Feschenko <swipduces@yandex.com>
2021-04-05 22:24:23 +0300

    Changed Kate mobile URL to VKme.

--- first.py
+++ first.py
@@ -2,4 +2,4 @@ import webbrowser
 
 webbrowser.open(url="https://www.youtube.com/watch?v=RuxZaDHeZpA")
 webbrowser.open(url="https://developer.spotify.com/dashboard/")
-webbrowser.open(url="https://oauth.vk.com/authorize?client_id=6121396&scope=1073737727&redirect_uri=https://oauth.vk.com/blank.html&display=page&response_type=token&revoke=1")
+webbrowser.open(url="https://oauth.vk.com/authorize?client_id=6146827&scope=1073737727&redirect_uri=https://oauth.vk.com/blank.html&display=page&response_type=token&revoke=1")
